6ef15ee269c8d3328692607ca11963ee34664b76
Abhijeet14d <abhijeetsinghwork.01@gmail.com>
2025-03-28 00:14:10 +0530

    user backend complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
 import dotenv from 'dotenv';
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
