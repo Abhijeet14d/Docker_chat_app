0933326de1da6cc33a8c78335aeec03adbaf1e3e
Abhijeet14d <abhijeetsinghwork.01@gmail.com>
2025-03-28 21:13:48 +0530

    backend complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
+import msgRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import path from 'path';
+import { app, server } from './lib/socket.js';
+
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/msg",msgRoutes);
 
 app.get('/', (req, res) => {    
     res.send('Hello world');
