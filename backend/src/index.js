54ea5d983ae3a5a2927355d5ec9040cfea935d0a
Abhijeet14d <abhijeetsinghwork.01@gmail.com>
2025-03-28 22:59:34 +0530

    frontend signup page

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
 import authRoutes from './routes/auth.route.js';
 import msgRoutes from './routes/message.route.js';
+
 import { connectDB } from './lib/db.js';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -9,12 +12,15 @@ dotenv.config();
 import path from 'path';
 import { app, server } from './lib/socket.js';
 
-const app = express();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/msg",msgRoutes);
@@ -23,7 +29,7 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 connectDB();
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`);
     
 })
